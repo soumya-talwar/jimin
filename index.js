bbb01ebf14fb4992e9479c5244a48eb06517fa11
Soumya Talwar <soumya.talwar97@gmail.com>
2022-03-08 13:47:48 +0530

    text to speech

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ server.listen(3000, () => {
 io.on("connection", socket => {
   socket.on("recording", text => {
     console.log("you spoke: " + text);
+    io.emit("respond", text);
   });
   socket.on("response", text => {
     console.log("jimin says: " + text);
@@ -46,26 +47,26 @@ var browser, page;
   });
 })();
 
-// const serial = require("serialport");
-// const port = new serial("/dev/cu.usbmodem14301", {
-//   baudRate: 9600
-// });
-// const reader = require("@serialport/parser-readline");
-// const parser = port.pipe(new reader());
-//
-//
-// require("dotenv").config();
-// const id = process.env.ACCOUNT_SID;
-// const token = process.env.AUTH_TOKEN;
-// const twilio = require("twilio")(id, token);
-//
-// parser.on("data", data => console.log(data));
-//
-// function whatsapp(content) {
-//   twilio.messages.create({
-//       from: "whatsapp:+14155238886",
-//       body: content,
-//       to: "whatsapp:+917982821106"
-//     })
-//     .then(message => console.log(message));
-// }
+const serial = require("serialport");
+const port = new serial("/dev/cu.usbmodem14301", {
+  baudRate: 9600
+});
+const reader = require("@serialport/parser-readline");
+const parser = port.pipe(new reader());
+
+
+require("dotenv").config();
+const id = process.env.ACCOUNT_SID;
+const token = process.env.AUTH_TOKEN;
+const twilio = require("twilio")(id, token);
+
+parser.on("data", data => console.log(data));
+
+function whatsapp(content) {
+  twilio.messages.create({
+      from: "whatsapp:+14155238886",
+      body: content,
+      to: "whatsapp:+917982821106"
+    })
+    .then(message => console.log(message));
+}
