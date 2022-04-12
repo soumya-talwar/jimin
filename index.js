4fbfafa20fa9cf3fe78eee2ee465541860e9c49e
Soumya Talwar <soumya.talwar97@gmail.com>
2022-04-12 21:09:40 +0530

    before electron

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const {
-  Server
-} = require("socket.io");
-const io = new Server(server);
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const server = http.createServer(app);
+// const {
+//   Server
+// } = require("socket.io");
+// const io = new Server(server);
 
-app.get("/", (request, response) => response.sendFile(__dirname + "/voice.html"));
-app.get("/capture", (request, response) => response.sendFile(__dirname + "/capture.html"));
+// app.get("/", (request, response) => response.sendFile(__dirname + "/voice.html"));
+// app.get("/capture", (request, response) => response.sendFile(__dirname + "/capture.html"));
+//
+// server.listen(3000, () => console.log("listening on :3000"));
+//
+// io.on("connection", socket => {
+//   socket.on("recording", async text => {
+//     console.log("you spoke: " + text);
+//     let response = await nlp.process("en", text);
+//     console.log(response);
+//     io.emit("respond", response.answer);
+//   });
+//   socket.on("response", text => {
+//     console.log("jimin says: " + text);
+//   });
+// });
 
-server.listen(3000, () => console.log("listening on :3000"));
+const say = require("say");
+say.speak("Hello Sawmiya, my name is Jimin. How are you doing?", "Daniel", 1.0);
 
-io.on("connection", socket => {
-  socket.on("recording", text => {
-    console.log("you spoke: " + text);
-    io.emit("respond", text);
-  });
-  socket.on("response", text => {
-    console.log("jimin says: " + text);
-  });
-});
+// const {
+//   dockStart
+// } = require("@nlpjs/basic");
+//
+// (async () => {
+//   const dock = await dockStart({
+//     use: ["Basic"]
+//   });
+//   const nlp = dock.get("nlp");
+//   await nlp.addCorpus("./train.json");
+//   await nlp.train();
+// })();
+//
+// const fs = require("fs");
+// const {
+//   NlpManager
+// } = require("node-nlp");
+//
+// const data = fs.readFileSync("model.nlp", "utf8");
+// const manager = new NlpManager();
+// manager.import(data);
+//
+// manager
+//   .process("hi")
+//   .then(result => console.log(result));
 
-const puppeteer = require("puppeteer");
-var browser, page;
-(async () => {
-  browser = await puppeteer.launch({
-    args: ["--use-fake-ui-for-media-stream"]
-  });
-  page = await browser.newPage();
-  await page.goto("http://localhost:3000/capture");
-  page.on("console", message => {
-    let text = message.text();
-    console.log(text);
-    if ((/^spouse/).test(text))
-      io.emit("listening", true);
-    else if ((/^not spouse/).test(text))
-      io.emit("listening", false);
-  });
-})();
+// const puppeteer = require("puppeteer");
+// var browser, page;
+// (async () => {
+//   browser = await puppeteer.launch({
+//     args: ["--use-fake-ui-for-media-stream"]
+//   });
+//   page = await browser.newPage();
+//   await page.goto("http://localhost:3000/capture");
+//   page.on("console", message => {
+//     let text = message.text();
+//     console.log(text);
+//     if ((/^spouse/).test(text))
+//       io.emit("listening", true);
+//     else if ((/^not spouse/).test(text))
+//       io.emit("listening", false);
+//   });
+// })();
 
 // const serial = require("serialport");
 // const port = new serial("/dev/cu.usbmodem14301", {
